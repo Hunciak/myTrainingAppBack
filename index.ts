670dadfea16f3699d1b19b01edc3e1496d163d02
Szymon <szymon.chyrza@gmail.com>
2023-06-09 16:40:39 +0200

    [BE] installed bcrypt and mysql2. Changed imports to variables and commented one import in file index.ts

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
-import express, {json} from 'express';
-import {handleError} from "./utils/errors";
-import cors from 'cors';
+const express = require('express');
+const cors = require('cors');
+// import {handleError} from "./utils/errors";
 
 const app = express();
 
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
-app.use(handleError)
-app.use(json());
+// app.use(handleError)
+app.use(express.json());
 app.listen(3001, '0.0.0.0', () => {
     console.log('Backend is ready on http://localhost:3001');
 })
